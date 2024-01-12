ec19cc00aaa9451742f6ca9831416d37fa4b5c11
JeMax <jemaxter25@gmail.com>
2024-01-12 00:39:09 -0500

    scroll-bar

--- src/Portfolio/Design/SectionTwo.jsx
+++ src/Portfolio/Design/SectionTwo.jsx
@@ -6,9 +6,9 @@ export const SectionTwo = () => {
   let number = 1; 
 
   return (
-    <section className='w-full h-auto pb-7 relative z-10 bg-slate-800/90 dark:bg-slate-200/90 rounded-3xl backdrop-blur-sm'>  
+    <section className='w-full h-auto relative z-10 bg-slate-800 dark:bg-slate-200 rounded-3xl backdrop-blur-sm'>  
         <h1 className='text-center text-2xl md:text-4xl lg:text-5xl p-9 font-bold dark:text-slate-900'>Proyectos</h1>
-        <swiper-container class="mySwiper" space-between="30" slides-per-view="2" navigation="true" pagination="true">
+        <swiper-container class="mySwiper" space-between="30" slides-per-view="2" navigation="true">
           {
             cardInfo.map( card => (
               <Card key={card.name + number++} card={card}></Card>
